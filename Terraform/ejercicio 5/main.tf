76eabb8f2b1973ff44e851d303823b7bb289d363
Maicol Garelli <MGarelli@geocom.com.uy>
2023-08-17 10:21:34 -0300

    insert main.ts

---
+++ Terraform/ejercicio 5/main.tf
@@ -0,0 +1,53 @@
+terraform {
+    required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "5.10.0"
+    }
+    }
+}
+
+provider "aws" {
+    region = "us-east-1"
+}
+resource "aws_eip" "wordpress-eip" {
+    tags = {
+        Name="wordpress-eip"
+        OWNER="edgaregonzalez@gmail.com"
+    }
+}
+resource "aws_eip_association" "serverwindows-eip" {
+    instance_id = aws_instance.serverwindows.id
+    allocation_id = aws_eip.wordpress-eip.id
+}
+resource "aws_security_group" "serverwindows-sg" {
+    name = "serverwindows-rdp-sg"
+    description = "acceso remoto para administracion del equipo"
+    vpc_id = "vpc-0e6f118a8dc74da8b"
+    ingress {
+        from_port = 3389
+        to_port = 3389
+        protocol = "tcp"
+        cidr_blocks = ["200.117.89.71/32"]
+    }
+egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+    }
+}
+resource "aws_instance" "serverwindows" {
+    ami = "ami-0fc682b2a42e57ca2"
+    instance_type = "t2.micro"
+    key_name = "devops-educacionit"
+    associate_public_ip_address = false
+    vpc_security_group_ids = [aws_security_group.serverwindows-sg.id]
+    subnet_id = "subnet-00aed494d19efbf81"
+
+    tags = {
+        Name="serverwindows"
+        OWNER="edgaregonzalez@gmail.com"
+    }
+}
